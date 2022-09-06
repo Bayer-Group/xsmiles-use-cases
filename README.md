ad95e0cd4ef89c5b3bb5ca06722c0d03c162d7d3
Henry Heberle <heberleh.public@gmail.com>
2022-09-06 17:02:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# XSMILES - JupyterLab example notebooks
+
+Examples of pipelines from *models & explanations* to *visualizations*.
+
+Available notebooks in `notebooks/`:
+
+ - Visualizing Gasteiger Charges (Simple example): `notebooks/atom_attributions_gasteiger_charges.ipynb`
+
+- Comparing LogD and bioconcetration factor attributions (Loading attributions from JSON): `TBD`.
+
+- Comparing LogP attributions from different methods (from ML Models to Attributions and Visualization): `notebooks/smiles_attributions_for_logp.ipynb`
+
+## Please Cite
+
+If you use XSMILES, the use cases, its code, or the generated explanations, please cite our article:
+
+**Article in preparation**, a preprint should appear mid September!
+
+```prose
+Heberle, H., Zhao, L., Schmidt, S., Wolf, T., & Heinrich, J. (2022). XSMILES: interactive visualization for molecules, SMILES and XAI scores. Article in preparation.
+```
+
+```BibTeX
+@article{Heberle2022XSMILES,
+author = {Heberle, Henry and Zhao, Linlin and Schmidt, Sebastian and Wolf, Thomas and Heinrich, Julian},
+doi = {},
+journal = {Article in preparation},
+month = {},
+number = {},
+pages = {},
+title = {{XSMILES: interactive visualization for molecules, SMILES and XAI scores}},
+volume = {},
+year = {2022}
+}
+```
+
+![JupyterLab Notebook](/screenshot.png)
+
+## XSMILES for Javascript, KNIME, and How to use it
+
+- [XSMILES main project](https://github.com/Bayer-Group/xsmiles)
+
+- [XSMILES for JupyterLab](https://github.com/Bayer-Group/xsmiles-jupyterlab)
+
+## How to run the notebook
+
+### Step 1 - Install general dependencies
+Create a new virtual environment and install the dependencies defined in `requirements.txt`:
+
+```bash
+# Make sure you have Python 3.7 installed
+python3.7 -m venv .venv_xsmiles_usecases
+source ./.venv_xsmiles_usecases/bin/activate # path to the created environment
+pip3 install -r requirements.txt
+```
+
+### Step 2 - Install XSMILES and CDDD
+
+Required: internet connection. The plugin will download RDkit MinimalLib when the JupyterLab notebook is loaded.
+
+To be able to visualize the XAI scores, you need to install XSMILES and CDDD.
+
+XSMILES for JupyterLab is available here: https://github.com/Bayer-Group/xsmiles-jupyterlab. You want to download the `xsmiles-0.2.2.dev0-py2.py3-none-any.whl` file from the releases page.
+
+An unofficial package for CDDD is available in this repository: `cddd-1.2.2-py3.none.any.whl`. We packed [CDDD scripts and the CDDD default_model](https://github.com/jrwnter/cddd) into a single package to use in the notebook more easily, as well as to use with our Substitution method (`attributor.py`). Please check the `smiles_attributions` notebook to see how to we use the package and import the CDDD default model. We created this package because in certain environments, Google Drive may be blocked by firewalls.
+
+<!-- If XSMILES cannot be installed through `pip` `requirements.txt`, check how to install it here: https://xsmiles-ipywidget. -->
+
+<!-- ```bash
+pip install xsmiles
+```
+
+or -->
+
+```bash
+pip install xsmiles-0.2.1.dev0-py2.py3-none-any.whl
+pip install cddd-1.2.2-py3.none.any.whl
+```
+
+Make sure `tensorboard==1.13.1` and `tensorflow==1.13.2` were installed through `requirements.txt`, CDDD depends on them.
+
+## Step 3 - Run JupyterLab
+
+Run JupyterLab and choose a notebook to explore:
+
+```bash
+jupyter lab notebooks
+```
---
+++ src/fp_xai/fp_xai.py
@@ -0,0 +1,256 @@
+# Code modified from  https://github.com/jensengroup/FP_RF_XAI
+# in reference to the original article: https://chemrxiv.org/engage/chemrxiv/article-details/622f215f968f5252a0ba640d
+# The original code is under MIT License - Copyright (c) 2022 Jensen Group
+
+import pickle
+import sys
+
+import pandas as pd
+import numpy as np
+
+import scipy.stats as ss
+
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import Descriptors
+from rdkit.Chem import AllChem
+from rdkit.Chem import DataStructs
+from rdkit.Chem import rdMolDescriptors
+from rdkit.Chem.Draw import SimilarityMaps
+
+from copy import deepcopy
+
+from scipy.stats import pearsonr
+
+
+def mol2fp(mol, radius=1, n_bits=1024):
+    bi = {}
+    fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=n_bits, bitInfo=bi)
+    arr = np.zeros((0,))
+    DataStructs.ConvertToNumpyArray(fp, arr)
+    return arr.astype(int)
+
+
+def mod_fp(mol, atom_idx, radius=2, n_bits=1024):
+    
+    info = {}
+    fps_morgan2 = AllChem.GetMorganFingerprintAsBitVect(mol, radius, n_bits, bitInfo=info)
+    
+    bitmap = [~DataStructs.ExplicitBitVect(n_bits) for x in range(mol.GetNumAtoms())]
+    
+    for bit, es in info.items():
+        for at1, rad in es:
+            if rad == 0: # for radius 0
+                bitmap[at1][bit] = 0
+            else: # for radii > 0
+                env = Chem.FindAtomEnvironmentOfRadiusN(mol, rad, at1)
+                amap = {}
+                submol = Chem.PathToSubmol(mol, env, atomMap=amap)
+                for at2 in amap.keys():
+                    bitmap[at2][bit] = 0
+    
+    new_fp = fps_morgan2 & bitmap[atom_idx]
+    return fps_morgan2, new_fp, info
+                    
+
+def mod_fp_exchange(mol, idx_atom, radius=1, dummy_atom_no=47, n_bits=1024):
+    info = {}
+    fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=n_bits, bitInfo=info)
+    
+    mol_cpy = deepcopy(mol)
+    mol_cpy.GetAtomWithIdx(idx_atom).SetAtomicNum(dummy_atom_no)
+    
+    new_info = {}
+    new_fp = AllChem.GetMorganFingerprintAsBitVect(mol_cpy, radius, nBits=n_bits, bitInfo=new_info)
+    
+    return fp, new_fp, info, new_info
+   
+
+def AAU_weights(mol, model, radius=1, n_bits=2048):
+
+    aau_weights = []
+    
+    info = {}
+    fps_morgan2 = AllChem.GetMorganFingerprintAsBitVect(mol, radius, n_bits, bitInfo=info)
+    
+    tree_predictions = []
+    for tree in model.estimators_:
+        tree_predictions.append(tree.predict(np.array([list(fps_morgan2)]))[0])
+
+    orig_uq = np.std(np.array(tree_predictions))
+
+    # get bits for each atom
+  
+    bitmap = [~DataStructs.ExplicitBitVect(n_bits) for x in range(mol.GetNumAtoms())]
+    for bit, es in info.items():
+        for at1, rad in es:
+            if rad == 0: # for radius 0
+                bitmap[at1][bit] = 0
+            else: # for radii > 0
+                env = Chem.FindAtomEnvironmentOfRadiusN(mol, rad, at1)
+                amap = {}
+                submol = Chem.PathToSubmol(mol, env, atomMap=amap)
+                for at2 in amap.keys():
+                    bitmap[at2][bit] = 0
+    # loop over atoms
+    for at1 in range(mol.GetNumAtoms()):
+        new_fp = fps_morgan2 & bitmap[at1]
+        tree_predictions = []
+        for tree in model.estimators_:
+            tree_predictions.append(tree.predict(np.array([list(new_fp)]))[0])
+        
+        new_uq = np.std(np.array(tree_predictions))
+        aau_weights.append(orig_uq-new_uq)
+
+    return aau_weights
+  
+
+
+def RDKit_normalized_weights(mol):
+    contribs_update = []
+    mol = Chem.AddHs(mol)
+    contribs = rdMolDescriptors._CalcCrippenContribs(mol)
+    contribs = [x for x,y in contribs]
+    #molecule = mols
+    for atom in mol.GetAtoms():
+        if atom.GetAtomicNum() == 1: 
+            index = atom.GetIdx()
+            for nabo in atom.GetNeighbors():
+                index_nabo= nabo.GetIdx() 
+                contribs[index_nabo] += contribs[index]
+
+    for atom in mol.GetAtoms():
+        if atom.GetAtomicNum() != 1:
+            index_update = atom.GetIdx()
+            contribs_update.append(contribs[index_update]) 
+    contribs_update = np.array(contribs_update)
+    Normalized_weightsRDkit = contribs_update.flatten()/np.linalg.norm(contribs_update.flatten())
+    return contribs_update, Normalized_weightsRDkit 
+
+
+def RDKit_bit_vector(mol, model, radius=1, n_bits=1024):
+    rdkit_contrib, _ = RDKit_normalized_weights(mol)
+    rdkit_bit_contribs = []
+    
+    ML_weights = []
+  
+    info = {}
+    fps_morgan2 = AllChem.GetMorganFingerprintAsBitVect(mol, radius, n_bits, bitInfo=info)
+
+    # Prediction with normal fingerprints
+    orig_pp = model.predict(np.array([list(fps_morgan2)]))[0]
+  
+    # get bits for each atom  
+    bitmap = [~DataStructs.ExplicitBitVect(n_bits) for x in range(mol.GetNumAtoms())]
+    for bit, es in info.items():
+        for at1, rad in es:
+            if rad == 0: # for radius 0
+                bitmap[at1][bit] = 0
+            else: # for radii > 0
+                env = Chem.FindAtomEnvironmentOfRadiusN(mol, rad, at1)
+                amap = {}
+                submol = Chem.PathToSubmol(mol, env, atomMap=amap)
+                for at2 in amap.keys():
+                    bitmap[at2][bit] = 0
+    # loop over atoms
+    for at1 in range(mol.GetNumAtoms()):
+        new_fp = fps_morgan2 & bitmap[at1]
+        new_pp = model.predict(np.array([list(new_fp)]))[0]
+        ML_weights.append(orig_pp-new_pp) 
+        removed_bits = []
+        for x in fps_morgan2.GetOnBits():
+            if x not in new_fp.GetOnBits():
+                removed_bits.append(x)
+        bit_contrib = 0
+        for bit in removed_bits:
+            for at, rad in info[bit]:
+                env = Chem.FindAtomEnvironmentOfRadiusN(mol, rad, at)
+                amap = {}
+                submol = Chem.PathToSubmol(mol, env, atomMap=amap)
+                for at2 in amap.keys():
+                    bit_contrib += rdkit_contrib[at2]
+        rdkit_bit_contribs.append(bit_contrib)
+    
+    return np.array(ML_weights), np.array(rdkit_contrib), np.array(rdkit_bit_contribs)
+
+
+def UAA_weights(mol, m, radius=2, n_bits=1024):
+
+    uaa_weights = []
+  
+    info = {}
+    fps_morgan2 = AllChem.GetMorganFingerprintAsBitVect(mol, radius, n_bits, bitInfo=info)
+
+    tree_predictions_og = []
+    for tree in m.estimators_:
+        tree_predictions_og.append(tree.predict(np.array([list(fps_morgan2)])))
+    
+    orig_pp = m.predict(np.array([list(fps_morgan2)]))[0]
+  
+    # get bits for each atom
+  
+    bitmap = [~DataStructs.ExplicitBitVect(n_bits) for x in range(mol.GetNumAtoms())]
+    for bit, es in info.items():
+        for at1, rad in es:
+            if rad == 0: # for radius 0
+                bitmap[at1][bit] = 0
+            else: # for radii > 0
+                env = Chem.FindAtomEnvironmentOfRadiusN(mol, rad, at1)
+                amap = {}
+                submol = Chem.PathToSubmol(mol, env, atomMap=amap)
+                for at2 in amap.keys():
+                    bitmap[at2][bit] = 0
+    # loop over atoms
+    for at1 in range(mol.GetNumAtoms()):
+        new_fp = fps_morgan2 & bitmap[at1]
+        tree_predictions = []
+        for tree in m.estimators_:
+            tree_predictions.append(tree.predict(np.array([list(new_fp)])))
+
+        weight_trees = np.array(tree_predictions_og)-np.array(tree_predictions)
+        sigma_weight = np.std(weight_trees)
+
+        uaa_weights.append(sigma_weight)
+        
+
+    #Normalized_weightsML = weights/np.linalg.norm(weights)
+    return np.array(uaa_weights).flatten()
+
+
+def get_weights_for_visualization(mol, model, radius=2, n_bits=1024):
+
+    ml_weights, atom_weights, FPA_weights = RDKit_bit_vector(mol, model, radius=radius, n_bits=n_bits)
+    
+    #atom_weights, _ = RDKit_normalized_weights(mol)
+    clogp = Chem.Crippen.MolLogP(mol)
+    fp = mol2fp(mol, radius=radius, n_bits=n_bits)
+    logp_pred = model.predict([fp])[0]
+    #print(clogp, logp_pred)
+    if np.sign(np.sum(ml_weights)) != np.sign(logp_pred):
+        print("ml weights: sign problem detected")
+        #ml_weights = -ml_weights
+    if np.sign(np.sum(FPA_weights)) != np.sign(clogp):
+        print("FPA weights: sign problem detected") 
+    
+    ml_weights = ml_weights*abs(logp_pred/np.sum(ml_weights))
+    FPA_weights = FPA_weights*abs(clogp/np.sum(FPA_weights))
+
+    return ml_weights, atom_weights, FPA_weights, logp_pred
+
+
+def get_contour_image(mol, weights, contour_step=0.06):
+    N_contours = (max(weights)-min(weights))/contour_step
+
+    fig = SimilarityMaps.GetSimilarityMapFromWeights(mol, weights, contourLines=round(N_contours))
+
+    return fig
+
+if __name__ == "__main__":
+    model_filename = sys.argv[1]
+    m = pickle.load(open(model_filename, 'rb'))
+    smiles = "CC[NH+](CC)[C@@H]1CCN(C(=O)N[C@@H]2CCCC2)C1"
+    mol = Chem.MolFromSmiles(smiles)
+    ml_weights, atom_weights, fpa_weights = get_weights_for_visualization(mol, m, radius=2, n_bits=2048)
+    fig = get_contour_image(mol, ml_weights)
+    fig.savefig("ml_weights_example.pdf", bbox_inches='tight')
+
